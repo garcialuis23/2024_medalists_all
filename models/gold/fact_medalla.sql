d586159254b343bf0ecb5d50dde3d5024ba0531c
garcialuis23 <garciadiazluis23@gmail.com>
2026-05-19 22:03:42 +0200

    actualizaciones probar el build

--- models/gold/fact_medalla.sql
+++ models/gold/fact_medalla.sql
@@ -8,22 +8,12 @@ select
     a.id_atleta,
     a.nombre                            as nombre_atleta,
     a.fecha_nacimiento,
-    a.sexo,
-    a.lugar_nacimiento,
-    a.region_nacimiento,
-    a.lat_nacimiento,
-    a.lon_nacimiento,
 
     -- país acreedor
     c.wikidata_id_pais                  as id_pais,
-    c.nombre                            as nombre_pais,
-    c.codigo_iso2                       as codigo_iso2_pais,
-    c.codigo_iso3                       as codigo_iso3_pais,
-    c.codigo_coi                        as codigo_coi_pais,
 
     -- delegación
     d.wikidata_id_delegacion            as id_delegacion,
-    d.nombre                            as nombre_delegacion,
 
     -- evento / disciplina / deporte
     e.id_evento,
@@ -33,13 +23,9 @@ select
 
     -- NUTS (solo atletas europeos; NULL para el resto)
     n.id_nuts3,
-    n.nombre_nuts3,
     n.id_nuts2,
-    n.nombre_nuts2,
     n.id_nuts1,
-    n.nombre_nuts1,
     n.id_nuts0,
-    n.nombre_nuts0                      as nombre_pais_nuts
 
 from {{ ref('silver_medalla') }} m
 join {{ ref('dim_atleta') }}        a on m.wikidata_id_atleta     = a.id_atleta
